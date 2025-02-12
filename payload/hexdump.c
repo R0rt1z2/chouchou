89e9be3baf52df48c0e86177b3f6849d18338562
R0rt1z2 <me@r0rt1z2.com>
2025-02-12 15:51:22 +0100

    Refactor hexdump functions
    Change-Id: Id05c38427a63ddccb0bf36301772a8d363daada3

--- payload/hexdump.c
+++ payload/hexdump.c
@@ -4,14 +4,14 @@
 #include <stddef.h>
 
 void uart_hexdump(const void *data, size_t size) {
-    size_t i;
-    size_t j;
+    size_t i, j;
+    const uint8_t *ptr = (const uint8_t *)data;
 
     for (i = 0; i < size; i += 16) {
-        uart_printf("%08x: ", i);
+        uart_printf("%08x: ", (uintptr_t)(ptr + i));
         for (j = 0; j < 16; j++) {
             if (i + j < size) {
-                uart_printf("%02x ", ((uint8_t *)data)[i + j]);
+                uart_printf("%02x ", ptr[i + j]);
             } else {
                 uart_printf("   ");
             }
@@ -20,7 +20,7 @@ void uart_hexdump(const void *data, size_t size) {
         uart_printf(" ");
         for (j = 0; j < 16; j++) {
             if (i + j < size) {
-                uint8_t c = ((uint8_t *)data)[i + j];
+                uint8_t c = ptr[i + j];
                 uart_printf("%c", (c >= 32 && c < 127) ? c : '.');
             }
         }
@@ -29,14 +29,14 @@ void uart_hexdump(const void *data, size_t size) {
 }
 
 void video_hexdump(const void *data, size_t size) {
-    size_t i;
-    size_t j;
+    size_t i, j;
+    const uint8_t *ptr = (const uint8_t *)data;
 
     for (i = 0; i < size; i += 16) {
-        video_printf("%08x: ", i);
+        video_printf("%08x: ", (uintptr_t)(ptr + i));
         for (j = 0; j < 16; j++) {
             if (i + j < size) {
-                video_printf("%02x ", ((uint8_t *)data)[i + j]);
+                video_printf("%02x ", ptr[i + j]);
             } else {
                 video_printf("   ");
             }
@@ -45,7 +45,7 @@ void video_hexdump(const void *data, size_t size) {
         video_printf(" ");
         for (j = 0; j < 16; j++) {
             if (i + j < size) {
-                uint8_t c = ((uint8_t *)data)[i + j];
+                uint8_t c = ptr[i + j];
                 video_printf("%c", (c >= 32 && c < 127) ? c : '.');
             }
         }
